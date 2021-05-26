60425f7bc6218e1930d2a47be269c1ffe3b2aa62
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-26 19:56:27 -0300

    Add get all on controller

--- backend/src/main/java/com/project/bootcamp/controller/StockController.java
+++ backend/src/main/java/com/project/bootcamp/controller/StockController.java
@@ -3,10 +3,11 @@ package com.project.bootcamp.controller;
 import com.project.bootcamp.model.dto.StockDTO;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/stock")
@@ -15,6 +16,25 @@ public class StockController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto){
         return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll(){
+        List<StockDTO> list = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Magazine Luiza");
+        dto.setPrice(100D);
+        dto.setVariation(10D);
+        dto.setDate(LocalDate.now());
+        list.add(dto);
+
+        return ResponseEntity.ok(list);
     }
 
 }
