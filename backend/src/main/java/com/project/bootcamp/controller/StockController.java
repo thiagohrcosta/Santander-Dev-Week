62fa9a50319c0a9f546ee7b4233951be1674b60e
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-26 19:49:58 -0300

    Add stock model

--- backend/src/main/java/com/project/bootcamp/controller/StockController.java
+++ backend/src/main/java/com/project/bootcamp/controller/StockController.java
@@ -1,4 +1,12 @@
 package com.project.bootcamp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/stock")
 public class StockController {
+
+
+
 }
---
+++ backend/src/main/java/com/project/bootcamp/model/dto/StockDTO.java
@@ -0,0 +1,13 @@
+package com.project.bootcamp.model.dto;
+
+import java.time.LocalDate;
+
+public class StockDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private LocalDate date;
+    private Double variation;
+
+}
