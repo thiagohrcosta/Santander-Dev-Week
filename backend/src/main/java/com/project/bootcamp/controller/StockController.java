becfa529266842b071c4da8467e7501f82463831
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-26 19:53:27 -0300

    Add post on controller

--- backend/src/main/java/com/project/bootcamp/controller/StockController.java
+++ backend/src/main/java/com/project/bootcamp/controller/StockController.java
@@ -1,5 +1,10 @@
 package com.project.bootcamp.controller;
 
+import com.project.bootcamp.model.dto.StockDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/stock")
 public class StockController {
 
-
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
 
 }
