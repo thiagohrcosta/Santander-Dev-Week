26508d20562eae2a60c95600039b2457d8efa13d
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-26 20:44:19 -0300

    Validations complete

--- backend/src/main/java/com/project/bootcamp/model/dto/StockDTO.java
+++ backend/src/main/java/com/project/bootcamp/model/dto/StockDTO.java
@@ -1,5 +1,9 @@
 package com.project.bootcamp.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
@@ -11,12 +15,16 @@ public class StockDTO {
     private String name;
 
     @NotNull
+    @DecimalMin(value="0.00")
+    @Digits(integer = 6, fraction = 2)
     private Double price;
 
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
 
     @NotNull
+    @Digits(integer = 3, fraction = 2)
     private Double variation;
 
     public Long getId() {
