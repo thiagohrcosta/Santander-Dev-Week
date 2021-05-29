ff4f6f1e87ad4277ca10c86d9ec0081d333b2aed
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-29 19:35:09 -0300

    Service created

--- backend/src/main/java/com/project/bootcamp/repository/StockRepository.java
+++ backend/src/main/java/com/project/bootcamp/repository/StockRepository.java
@@ -1,6 +1,10 @@
 package com.project.bootcamp.repository;
 
+import com.project.bootcamp.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StockRepository extends JpaRepository {
+@Repository
+public interface StockRepository extends JpaRepository <Stock, Long>{
+
 }
---
+++ backend/src/main/java/com/project/bootcamp/service/StockService.java
@@ -0,0 +1,8 @@
+package com.project.bootcamp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+    
+}
